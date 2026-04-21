45e11ef7351bde1ff1b189fcf3bdb52ff2612ba7
lh <lhillen@oas.de>
2026-04-21 10:56:54 +0200

    feat: reworked logging output to oop approach

--- src/compressify.py
+++ src/compressify.py
@@ -1,60 +1,114 @@
 import argparse as args
 import datetime as dt
+from output import Console
+from pathlib import Path
+
 
 def get_arguments() -> args.Namespace:
-    parser = args.ArgumentParser("compressify", 
-                                  description="CLI tool for compressing pdf files locally.",)
+    parser = args.ArgumentParser(
+        "compressify",
+        description="CLI tool for compressing pdf files locally.",
+    )
 
-    parser.add_argument("source", 
-                        type=str,
-                        help="source path of the file or directory that should be compressed.")
-    
-    parser.add_argument("target", 
-                        type=str,
-                        help="target path of the compressed file or files.")
-    
-    parser.add_argument("-v", "--verbose", 
-                        action="store_true", 
-                        dest="verbose", 
-                        help="enable verbose output in the console" )
-    
-    parser.add_argument("-r", "--recursive",
-                        action="store_true",
-                        dest="recursive",
-                        help="enable recursive file search when compressing a directory")
-    
-    parser.add_argument("-t", "--timestamps",
-                        action="store_true",
-                        dest="timestamps",
-                        help="enable timestamps in verbose console output")
-    
-    parser.add_argument("-l", "--level", 
-                        nargs=1, 
-                        dest="level", 
-                        default=9, 
-                        type=int,
-                        metavar="VALUE",
-                        help="specify the level of compression applied to targeted files")
-    
-    parser.add_argument("-i", "--imagec",
-                        dest="image_compression",
-                        default=100,
-                        type=int,
-                        metavar="VALUE",
-                        help="percentual compression of images in targeted files")
-    
+    parser.add_argument(
+        "source",
+        type=str,
+        help="source path of the file or directory that should be compressed.",
+    )
+
+    parser.add_argument(
+        "target", type=str, help="target path of the compressed file or files."
+    )
+
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        dest="recursive",
+        help="enable recursive file search when compressing a directory",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--filter",
+        nargs=1,
+        dest="filter",
+        default=None,
+        type=str,
+        metavar="REGEX",
+        help="specify a filter expression for the targeted files.",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--level",
+        nargs=1,
+        dest="level",
+        default=9,
+        type=int,
+        metavar="0-9",
+        help="specify the level of compression applied to targeted files",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--imagec",
+        dest="image_compression",
+        default=100,
+        type=int,
+        metavar="0-100",
+        help="percentual compression of images in targeted files",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="verbose",
+        help="enable verbose output in the console",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--timestamps",
+        action="store_true",
+        dest="timestamps",
+        help="enable timestamps in verbose console output",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--no-color",
+        action="store_true",
+        dest="no_color",
+        help="enable colorized text in verbose console output",
+    )
+
     return parser.parse_args()
 
 
-def print_execution_header(arguments: args.Namespace) -> None:
-    print("Running compressify with arguments:")
+def print_execution_header(console: Console, arguments: args.Namespace) -> None:
+    console.log_info(f"Running compressify with arguments:")
     for ident, value in arguments._get_kwargs():
-        print(f"> {ident}: {value}")
+        console.log_raw(f"> {ident}: {value}")
 
 
 def main() -> None:
+    console: Console = Console()
     arguments = get_arguments()
-    print(print_execution_header(arguments))
+
+    # Configure logging in console
+    console.toggle_output(arguments.verbose)
+    console.toggle_colorized(not arguments.no_color)
+    console.toggle_timestamps(arguments.timestamps)
+    print_execution_header(console, arguments)
+
+    # Collect files
+    console.log_info("Collecting files")
+
+    # compress at level
+
+    # compress images
 
 
 if __name__ == "__main__":
--- src/output.py
+++ src/output.py
@@ -1,89 +1,93 @@
-import importlib.metadata as meta
 import datetime as dt
 
-_scopes: list[str] = list()
-_info: str = "[INFO]"
-_warn: str = "[WARN]"
-_show_timestamps: bool = False
-_show_colorized: bool = False
+INFO: str = "[INFO]"
+WARN: str = "[WARN]"
+LEVEL_CHAR_LEN: int = len(INFO)
+HEADER: str = "\033[95m"
+OKBLUE: str = "\033[94m"
+OKCYAN: str = "\033[96m"
+OKGREEN: str = "\033[92m"
+WARNING: str = "\033[93m"
+FAIL: str = "\033[91m"
+ENDC: str = "\033[0m"
+BOLD: str = "\033[1m"
+UNDERLINE: str = "\033[4m"
 
+class Console:
+    _scopes: list[str] = list()
+    _scopes_char_len: int = 0
+    _show_timestamps: bool = False
+    _show_colorized: bool = False
+    _show_output: bool = False
 
-HEADER = "\033[95m"
-OKBLUE = "\033[94m"
-OKCYAN = "\033[96m"
-OKGREEN = "\033[92m"
-WARNING = "\033[93m"
-FAIL = "\033[91m"
-ENDC = "\033[0m"
-BOLD = "\033[1m"
-UNDERLINE = "\033[4m"
+    # Pushes a string as a scope into the output
+    def push_scope(self, scope: str):
+        self._scopes.append(scope)
+        self._scopes_char_len += len(scope)
 
+    # Pops the last scope from the output
+    def pop_scope(self):
+        scope = self._scopes.pop()
+        self._scopes_char_len -= len(scope)
 
-# Pushes a string as a scope into the output
-def push_scope(scope: str):
-    _scopes.append(scope)
+    def toggle_output(self, toggle: bool):
+        self._show_output = toggle
 
+    # Enable or disable timestamps in the console output
+    def toggle_timestamps(self, toggle: bool):
+        self._show_timestamps = toggle
 
-# Pops the last scope from the output
-def pop_scope():
-    _scopes.pop()
+    # Enable or disable colors in the console output
+    def toggle_colorized(self, toggle: bool):
+       self. _show_colorized = toggle
 
+    # Logs a string with the current configuration into the output console as raw text.
+    def log_raw(self, message: str):
+        if not self._show_output:
+            return
 
-# Enable or disable timestamps in the console output
-def toggle_timestamps(toggle: bool):
-    global _show_timestamps
-    _show_timestamps = True
+        print(LEVEL_CHAR_LEN * " ", end=" ")
+        if (self._scopes_char_len > 0):
+            print(self._scopes_char_len * " ", end=" ")
 
+        print(message)
 
-# Enable or disable colors in the console output
-def toggle_colorized(toggle: bool):
-    global _show_colorized
-    _show_colorized = toggle
+    # Logs a string with the current configuration into the output console as info.
+    def log_info(self, message: str):
+        if not self._show_output:
+            return
 
+        if self._show_timestamps:
+            print(dt.datetime.now().strftime("%H:%m:%S:%f"), end=" ")
 
-# Logs a string with the current configuration into the output console as info.
-def log_info(message: str):
+        if self._show_colorized:
+            print(OKCYAN, end="")
 
-    if _show_timestamps:
-        print(dt.datetime.now().strftime("%H:%m:%S:%f"), end=" ")
+        print(INFO, end=" ")
 
-    if _show_colorized:
-        print(OKCYAN, end="")
+        if self._show_colorized:
+            print(ENDC, end="")
 
-    print(_info, end=" ")
+        for scope in self._scopes:
+            print(f"({scope})", end=" ")
 
-    if _show_colorized:
-        print(ENDC, end="")
+        print(message)
 
-    for scope in _scopes:
-        print(f"({scope})", end=" ")
+    # Logs a string with the current configuration into the output console as warn.
+    def log_warn(self, message: str):
 
-    print(message)
+        if not self._show_output:
+            return
 
+        if self._show_timestamps:
+            print(dt.datetime.now().strftime("%H:%m:%S:%f"), end=" ")
 
-# Logs a string with the current configuration into the output console as warn.
-def log_warn(message: str):
-    
-    if _show_timestamps:
-        print(dt.datetime.now().strftime("%H:%m:%S:%f"), end=" ")
-        
-    if _show_colorized:
-        print(WARNING, end="")
+        if self._show_colorized:
+            print(WARNING, end="")
 
-    print(_warn, end=" ")
+        print(WARN, end=" ")
 
-    if _show_colorized:
-        print(ENDC, end="")
-        
-    print(message)
+        if self._show_colorized:
+            print(ENDC, end="")
 
-
-if __name__ == "__main__":
-    toggle_colorized(True)
-    toggle_timestamps(True)
-    log_info("Hello World")
-    log_warn("Hello World")
-    push_scope("Now Scope")
-    log_info("Hello World")
-    pop_scope()
-    log_info("Hello World")
+        print(message)
